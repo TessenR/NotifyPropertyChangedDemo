f469a9594163a05ced0cc2b192f0e73b0c74af77
Andrey.Dyatlov <Andrey.Dyatlov@jetbrains.com>
2020-12-02 02:13:12 +0300

    Demo generator implementing INotifyProperty changed interface based on naming convention
    Any type implementing INotifyPropertyChanged interface directly will be extended with a partial declaration containing the PropertyChanged event and properties raising it for any field named *BackingField

---
+++ NotifyPropertyChangedGenerator/NotifyPropertyChangedGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace NotifyPropertyChangedGenerator
+{
+  [Generator]
+  public class NotifyPropertyChangedGenerator : ISourceGenerator
+  {
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+      var compilation = context.Compilation;
+      var notifyInterface = compilation.GetTypeByMetadataName("System.ComponentModel.INotifyPropertyChanged");
+
+      foreach (var syntaxTree in compilation.SyntaxTrees)
+      {
+        var semanticModel = compilation.GetSemanticModel(syntaxTree);
+        var immutableHashSet = syntaxTree.GetRoot()
+          .DescendantNodesAndSelf()
+          .OfType<ClassDeclarationSyntax>()
+          .Select(x => semanticModel.GetDeclaredSymbol(x))
+          .OfType<ITypeSymbol>()
+          .Where(x => x.Interfaces.Contains(notifyInterface))
+          .ToImmutableHashSet();
+
+        foreach (var typeSymbol in immutableHashSet)
+        {
+          var source = GeneratePropertyChanged(typeSymbol);
+          context.AddSource($"{typeSymbol.Name}.Notify.cs", source);
+        }
+      }
+    }
+
+    private string GeneratePropertyChanged(ITypeSymbol typeSymbol)
+    {
+      return $@"
+using System.ComponentModel;
+
+namespace {typeSymbol.ContainingNamespace}
+{{
+  partial class {typeSymbol.Name}
+  {{
+    {GenerateProperties(typeSymbol)}
+    public event PropertyChangedEventHandler? PropertyChanged;
+  }}
+}}";
+    }
+
+    private static string GenerateProperties(ITypeSymbol typeSymbol)
+    {
+      var sb = new StringBuilder();
+      var suffix = "BackingField";
+
+      foreach (var fieldSymbol in typeSymbol.GetMembers().OfType<IFieldSymbol>()
+        .Where(x=>x.Name.EndsWith(suffix)))
+      {
+        var propertyName = fieldSymbol.Name[..^suffix.Length];
+        sb.AppendLine($@"
+    public {fieldSymbol.Type} {propertyName}
+    {{
+      get => {fieldSymbol.Name};
+      set
+      {{
+        {fieldSymbol.Name} = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof({propertyName})));
+      }}
+    }}");
+      }
+
+      return sb.ToString();
+    }
+  }
+}
--- NotifyPropertyChangedLiveDemo/CarModel.cs
+++ NotifyPropertyChangedLiveDemo/CarModel.cs
@@ -2,44 +2,12 @@
 
 namespace NotifyPropertyChangedLiveDemo
 {
-  public class CarModel : INotifyPropertyChanged
+  public partial class CarModel : INotifyPropertyChanged
   {
     private double SpeedKmPerHourBackingField;
     private int NumberOfDoorsBackingField;
     private string ModelBackingField = "";
 
     public void SpeedUp() => SpeedKmPerHour *= 1.1;
-
-    public double SpeedKmPerHour
-    {
-      get => SpeedKmPerHourBackingField;
-      set 
-      {
-        SpeedKmPerHourBackingField = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SpeedKmPerHour)));
-      }
-    }
-
-    public int NumberOfDoors
-    {
-      get => NumberOfDoorsBackingField;
-      set 
-      {
-        NumberOfDoorsBackingField = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NumberOfDoors)));
-      }
-    }
-
-    public string Model
-    {
-      get => ModelBackingField;
-      set 
-      {
-        ModelBackingField = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Model)));
-      }
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
   }
 }
